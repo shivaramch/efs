3d6480dba92cb9b12feda95a7476897f0bea0b88
Shiva Ram Chennapragada <schennapragada@unomaha.edu>
2016-09-15 23:43:10 -0500

    adding efs to github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,13 @@
                 font-size: 20px;
                 color: #9E2424;
             }
+            <!--.widget{
+                margin: 50px;
+                padding: 50px;
+                width: 50%;
+                display: table-footer-group;
+                position: inherit;
+            }-->
         </style>
     </head>
     <body>
@@ -59,5 +66,9 @@
                 <div>Come back later for more!</div>
             </div>
         </div>
+    <!--<div class="widget">
+        <iframe src="http://tsw.forexprostools.com?&forex=1,2,3,5,7,9,10&commodities=8830,8836,8831,8849,8833,8862,8832&indices=175,166,172,27,167,40830,179&stocks=284,280,282,287,286,288,295&tabs=1,2,3,4" width="317" height="467"></iframe><div class="poweredBy" style="font-family:arial,helvetica,sans-serif; direction:ltr;"><span style="font-size: 11px;color: #333333;text-decoration: none;">Technical Summary Widget Powered by <a href="http://www.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com</a></span></div>
+        <iframe frameborder="0" scrolling="no" height="375" width="197" allowtransparency="true" marginwidth="0" marginheight="0" src="http://tools.investing.com/currency-converter/index.php?from=17&to=12"></iframe><br /><table width="197"><tr><td><span style="font-size: 11px;color: #333333;text-decoration: none;">The Currency Converter is powered by <a href="http://www.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com</a></span></td></tr></table>
+    </div>-->
     </body>
 </html>
